401b1bb5ffab76f29d9adb0502683fc862da8470
unknown <torosdepilauco@gmail.com>
2024-01-03 20:29:37 -0300

    avances 03/01/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent     } from './app.component';
-import { HomeComponent    } from './pages/home/home.component';
-import { AsideComponent   } from './pages/aside/aside.component';
+import { AppRoutingModule   } from './app-routing.module';
+import { AppComponent       } from './app.component';
+import { HomeComponent      } from './pages/home/home.component';
+import { AsideComponent     } from './pages/aside/aside.component';
 
-import { SidebarModule    } from 'primeng/sidebar';
-import { ButtonModule     } from 'primeng/button';
-import { MenubarModule    } from 'primeng/menubar';
-import { InGameComponent  } from './pages/in-game/in-game.component';
-import { DataViewModule   } from 'primeng/dataview';
-import { RippleModule     } from "primeng/ripple";
+import { SidebarModule      } from 'primeng/sidebar';
+import { ButtonModule       } from 'primeng/button';
+import { MenubarModule      } from 'primeng/menubar';
+import { InGameComponent    } from './pages/in-game/in-game.component';
+import { DataViewModule     } from 'primeng/dataview';
+import { RippleModule       } from "primeng/ripple";
+import { DialogModule       } from 'primeng/dialog';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FormsModule        } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputSwitchModule  } from 'primeng/inputswitch';
+import { ListboxModule      } from 'primeng/listbox';
+import { CardModule         } from 'primeng/card';
+import { DividerModule      } from 'primeng/divider';
+import { TableModule        } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -29,7 +38,16 @@ import { RippleModule     } from "primeng/ripple";
     ButtonModule,
     MenubarModule,
     DataViewModule,
-    RippleModule
+    RippleModule,
+    DialogModule,
+    SelectButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputSwitchModule,
+    ListboxModule,
+    CardModule,
+    DividerModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/in-game/in-game.component.ts
+++ src/app/pages/in-game/in-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef,AfterViewInit, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-in-game',
   templateUrl: './in-game.component.html',
@@ -8,8 +9,12 @@ import { Component, ViewChild, ElementRef,AfterViewInit, OnInit } from '@angular
 export class InGameComponent implements AfterViewInit, OnInit{
   @ViewChild('myCanvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
 
+  public gameJsonArray : any[] = [];
+
   //-- Cuartos
   public period      : string = "1";
+
+  public twentyFour  : string = "24";
 
   //-- Nombres de los equipos
   public homeName    : string = "TEAM A";
@@ -28,11 +33,29 @@ export class InGameComponent implements AfterViewInit, OnInit{
   public inGameStart : boolean = false;
   private lastMinute : boolean = true;
   private endQuarter : boolean = false;
+
+  public scoreModal  : boolean = false;
+  public playerData  : any[] = [];
   public homePlayers : any[] = [
     {
-      id : 1,
-      name : 'Player 1',
-      in_game : true
+      id       : 1,
+      name     : 'Rodolfo Soto Figueroa',
+      age      : 28,
+      position : 'Pivot',
+      jersey_n : 36,
+      in_game  : true,
+      rebounds : 0,
+      points   : 0,
+      assists  : 0,
+      doubles  : 0,
+      three    : 0,
+      gen_at   : 0,
+      doubles_at  : 0,
+      three_at    : 0,
+      general_percent : "0%",
+      three_percent   : "0%",
+      double_percent  : "0%",
+      minutes_played  : 0
     },
     {
       id : 2,
@@ -52,73 +75,73 @@ export class InGameComponent implements AfterViewInit, OnInit{
     {
       id : 5,
       name : 'Player 5',
-      in_game : true
-    },
-    {
-      id : 6,
-      name : 'Player 6',
-      in_game : true
-    },
-    {
-      id : 7,
-      name : 'Player 7',
-      in_game : true
-    },
-    {
-      id : 8,
-      name : 'Player 8',
-      in_game : true
-    },
+      in_game : false
+    }
 
   ];
   public awayPlayers : any[] = [
     {
-      id : 1,
+      id : 6,
       name : 'Player 1',
       in_game : true
     },
     {
-      id : 2,
+      id : 7,
       name : 'Player 2',
       in_game : true
     },
     {
-      id : 3,
+      id : 8,
       name : 'Player 3',
       in_game : true
     },
     {
-      id : 4,
+      id : 9,
       name : 'Player 4',
-      in_game : true
+      in_game : false
     },
     {
-      id : 5,
+      id : 10,
       name : 'Player 5',
       in_game : true
     },
-    {
-      id : 6,
-      name : 'Player 6',
-      in_game : true
-    },
-    {
-      id : 7,
-      name : 'Player 7',
-      in_game : true
-    },
-    {
-      id : 8,
-      name : 'Player 8',
-      in_game : true
-    },
-
   ];
+
+  public ballInOptions : any[] = [{label: "Scores", value: "scores" }, {label: "Ball out", value: 'ballout'}];
+
+  public ballInOption  : string = "scores";
+
+  public teamsOptions  : any[] = [{label: this.homeName, value: 'home' }, {label: this.awayName, value: 'away'}];
+
+  public scoreTeam     : string = "home";
+
+  public scoreOptions  : any[] = [{label: "3 pts.", value: 3 }, {label: "2 pts.", value: 2 }, {label: "1 pt.", value: 1 }];
+
+  public scoreOption   : string = "2";
+
+  public scorePlayer   : any = {};
+
+  public extraShot     : boolean = false;
+
+  private x : number = 0;
+  private y : number = 0;
+
+  public playerInfo : boolean = false;
+
+  public selectedPlayer : any = {};
 
   ngOnInit(){
 
     setInterval( () => {
       if ( this.inGameStart == true ){
+
+        //--
+        this.twentyFour = (parseInt(this.twentyFour) - 1) + "";
+
+        if ( parseInt(this.twentyFour) == 0 ){
+          this.inGameStart = false;
+          this.twentyFour  = "24"; 
+        }
 
         if ( this.lastMinute == true ){
           if ( parseInt(this.timerMin) == 0 && parseInt(this.timerSec) == 0 ){
@@ -151,7 +174,8 @@ export class InGameComponent implements AfterViewInit, OnInit{
             }
 
             if ( parseInt(this.timerMin) == 0 && parseInt(this.timerSec) == 0 ){
-              this.lastMinute = true;            
+              this.lastMinute = true;      
+              this.timerSec = "59";      
             }
             else{
               this.timerSec = "59";
@@ -171,13 +195,9 @@ export class InGameComponent implements AfterViewInit, OnInit{
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
 
-      ctx.fillStyle = 'red'; // Cambia aquí por el color deseado
-      ctx.beginPath();
-      ctx.arc(x, y, 5, 0, Math.PI * 2); // Cambia el radio si deseas cambiar el tamaño del círculo
-      ctx.fill();
-      ctx.closePath();
-
-      console.log(`Coordenada X: ${x}, Coordenada Y: ${y}`);
+      this.x = x;
+      this.y = y;
+      this.scores( x, y);
     });
 
     const ctx = this.canvas.nativeElement.getContext('2d')!;
@@ -199,4 +219,215 @@ export class InGameComponent implements AfterViewInit, OnInit{
   pauseGame(){
     this.inGameStart = false;
   }
+
+  public scores( x : any, y : any ){
+    this.scoreModal = true;
+    
+
+    console.log(`Coordenada X: ${x}, Coordenada Y: ${y}`);
+  }
+
+  public setScorePlayer( player : any ){
+    this.scorePlayer = player;
+  }
+
+  public saveInPlayData(){
+
+    const ctx = this.canvas.nativeElement.getContext('2d')!;
+    let jsonData = {};
+    let color    = '';
+    if ( this.scoreTeam == "home" ){
+      color = "#42579E";
+    }
+    else{
+      color = '#FF914D';
+    }
+    if ( this.ballInOption == "scores" ){
+
+      ctx.fillStyle = color; // Cambia aquí por el color deseado
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, 5, 0, Math.PI * 2); // Cambia el radio si deseas cambiar el tamaño del círculo
+      ctx.fill();
+      ctx.closePath();
+
+      jsonData = {
+        action    : 'score_action',
+        team      : this.scoreTeam,
+        addition  : this.scoreOption,
+        player_id : this.scorePlayer.id,
+        extra     : this.extraShot,
+        scores    : true
+      }
+
+      this.changeScore( parseInt(this.scoreOption), this.scoreTeam );
+      
+    }
+    else{
+      // Tamaño de la "x"
+      const size = 5; // Cambia el tamaño de la "x" si lo deseas
+
+      // Color de la "x"
+      ctx.fillStyle = color; // Cambia aquí por el color deseado
+
+      // Dibujar la "x"
+      ctx.beginPath();
+      ctx.moveTo(this.x - size, this.y - size);
+      ctx.lineTo(this.x + size, this.y + size);
+      ctx.moveTo(this.x + size, this.y - size);
+      ctx.lineTo(this.x - size, this.y + size);
+      ctx.stroke();
+      ctx.closePath();
+
+      jsonData = {
+        action    : 'score_action',
+        team      : this.scoreTeam,
+        addition  : this.scoreOption,
+        player_id : this.scorePlayer.id,
+        extra     : this.extraShot,
+        scores    : false
+      }
+    }
+
+    this.updatePlayerData(parseInt(this.scoreOption), this.ballInOption, this.scoreTeam, this.scorePlayer.id);
+
+    this.gameJsonArray.push(jsonData);
+    this.scoreModal = false;
+
+    this.cleanData();
+    
+  }
+
+  private updatePlayerData( points : number, scores : string , scoreTeam : string, player_id : number ){
+
+    if ( scoreTeam == "home" ){
+      for ( let i = 0 ; i < this.homePlayers.length ; i ++ ){
+        if ( player_id == this.homePlayers[i].id ){
+          if ( scores == "scores" ){
+            this.homePlayers[i].points += points;
+            if ( points == 2 ){
+              this.homePlayers[i].doubles += 2;
+
+            }
+            else if ( points == 3 ){
+              this.homePlayers[i].three  += 3;
+            }
+          }
+          if ( points == 2 ){
+            this.homePlayers[i].doubles_at += 2; 
+            this.homePlayers[i].gen_at     += 2; 
+            this.homePlayers[i].double_percent  = this.getPercent(this.homePlayers[i].doubles, this.homePlayers[i].doubles_at) + "%";
+            this.homePlayers[i].general_percent = this.getPercent(this.homePlayers[i].points, this.homePlayers[i].gen_at) + "%";
+          }
+          else if ( points == 3 ){
+            this.homePlayers[i].three_at += 3; 
+            this.homePlayers[i].gen_at   += 3; 
+            this.homePlayers[i].three_percent   = this.getPercent(this.homePlayers[i].three, this.homePlayers[i].three_at) + "%";
+            this.homePlayers[i].general_percent = this.getPercent(this.homePlayers[i].points, this.homePlayers[i].gen_at) + "%";
+          }
+
+        }
+      }
+    }
+    else{
+
+    }
+  }
+
+  private getPercent( total : number, attempted : number ){
+    let percent = (total/attempted) * 100;
+
+    return percent;
+  }
+
+  private changeScore( addition : number, team : string ){
+    if ( team == "home" ){
+      this.homeGoals = (parseInt(this.homeGoals) + addition) + "";
+      if ( parseInt(this.homeGoals) < 10 ){
+        this.homeGoals = "0" + this.homeGoals;
+      }
+    }
+
+    else if ( team == "away" ){
+      this.awayGoals = (parseInt(this.awayGoals) + addition) + "";
+      if ( parseInt(this.awayGoals) < 10 ){
+        this.awayGoals = "0" + this.awayGoals;
+      }
+    }
+  }
+
+  private cleanData(){
+    this.ballInOption = "scores";
+    this.scoreTeam    = "home";
+    this.scoreOption  = "";
+
+    this.scorePlayer  = {};
+    this.extraShot    = false;
+  }
+
+  public showPlayer( player : any ){
+    this.playerData     = [];
+    this.playerInfo     = true;
+    this.selectedPlayer = player;
+
+    this.playerData.push(
+      {
+        item  : 'Full name',
+        value : player.name 
+      },
+      {
+        item  : 'Age',
+        value : player.age 
+      },
+      {
+        item  : 'Position',
+        value : player.position 
+      },
+      {
+        item  : 'N°',
+        value : player.jersey_n 
+      },
+      {
+        item  : 'Points',
+        value : player.points 
+      },
+      {
+        item  : '2 point',
+        value : player.doubles 
+      },
+      {
+        item  : '3 point',
+        value : player.three 
+      },
+      {
+        item  : 'Score percent',
+        value : player.general_percent 
+      },
+      {
+        item  : '3 point percent',
+        value : player.three_percent 
+      },
+      {
+        item  : '2 point percent',
+        value : player.double_percent 
+      },
+      {
+        item  : 'Minutes played',
+        value : player.minutes_played 
+      },
+      
+    );
+
+
+  }
+
+  public resetTF( num : string ){
+    this.twentyFour = num;
+  }
 }
+
+
+
+
+
+
+
